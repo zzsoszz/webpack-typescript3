4544d66c90c1f5bce92092c9aa428865802cf63d
hyq2015 <18030638805@163.com>
2016-11-25 18:04:07 +0800

    update

--- main.ts
+++ main.ts
@@ -1,19 +1,17 @@
 import {Greet} from "./greet";
 import {Hello} from "./hello";
 import * as $  from "jquery";
-function User(){
-    this.name="aaaa";
-    this.say=function(){
-        console.log("user:"+this.name);
-    };
-}
 $(document).ready(function(){
     $("#btn").click(function(){
-            require.ensure(['./greet'], function(require) {
+            require.ensure(['./greet',"./hello"], function(require) {
                 //{Greet:{new():Greet}}  {Greet:new()=>Greet}
-                var greet =<{Greet:{new():Greet}}>require('./greet');
-                var  bbb=new greet.Greet();
-                bbb.sayHi();
+                var greetmodule =<{Greet:{new():Greet}}>require('./greet');
+                var  greet=new greetmodule.Greet();
+                greet.sayHi();
+                
+                var hellomodule =<{Hello:{new():Hello}}>require('./hello');
+                var hello=new hellomodule.Hello();
+                hello.sayHello();
             });
     });
 });
